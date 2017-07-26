6a04bbbeb14214e08f47211c188a6eecbb74a5e5
Roman-Onishencko <roman951t@gmail.com>
2017-07-26 23:41:42 +0300

    Add files via upload

--- src/MovieById.js
+++ src/MovieById.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-// import MovieAPI from './MovieAPI';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actions from './actions';
+import PropTypes from 'prop-types';
+import './MovieById.css';
 
 const MovieById = (props) => {
-  // const movie = MovieAPI.get(
-  //   parseInt(props.match.params.number, 10)
-  // )
-  // if (!movie) {
-  //   return <div>Sorry, but the movie was not found</div>
-  // }
-  // return (
-  //   <div>
-  //     <h1>{movie.title} (#{movie.number})</h1>
-  //     <h2>{movie.overview}</h2>
-  //   </div>
-  // )
-  return <span></span>
+
+  const wishlist = props.wishList.get('wishList');
+  const movieId = parseInt(props.match.params.number, 10);
+  const currentMovie = wishlist.filter( (val) => val.id === movieId );
+  const movieObj = currentMovie.get(0);
+  return (
+
+    props.wishList.get('wishList').size > 0 && <div className="current">
+      <p className="current__title">{movieObj.title}</p>
+      <img
+        className="current__logo"
+        src={movieObj.poster_path}
+        alt="logo"
+      />
+      <p className="current__overview">{movieObj.overview}</p>
+      <p className="current__date">{movieObj.release_date}</p>
+    </div>
+  )
 };
 
-export default MovieById;
+MovieById.propTypes = {
+  wishList: PropTypes.array,
+};
+
+export default connect(
+  state => ({
+    wishList: state.wishList,
+  }),
+  dispatch => bindActionCreators(actions, dispatch)
+)(MovieById);
